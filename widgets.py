8f533912163a4f775b58e81603104c2c00133a42
lenolib <engnutt@gmail.com>
2017-01-24 21:36:08 +0100

    More structure, plot widgets together. Combobox

--- widgets.py
+++ widgets.py
@@ -1,5 +1,8 @@
 from __future__ import print_function, division
 
+import os
+import sys
+
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 
@@ -39,21 +42,53 @@ from PyQt4.QtGui import (
     QSlider,
     QLineEdit,
     QCompleter,
+    QComboBox,
 )
 
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib
 from operator import itemgetter
 
+import logging
+if not logging.getLogger().handlers:
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)-6s [%(name)s]: %(message)s'
+    )
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 
 class_properties = {
-    matplotlib.lines.Line2D: ['linewidth', 'label']
+    matplotlib.lines.Line2D: ['linewidth', 'label', 'alpha', 'linestyle'],
+
 }
 
+from collections import OrderedDict
+
+linestyle_dict =  OrderedDict([
+    ('solid line', '-'),
+    ('dashed line', '--'),
+    ('dash-dotted line', '-.'),
+    ('dotted line', ':'),
+    ('draw nothing', 'None'),
+])
+
+
 property_2_widget_factory = {
+    'alpha': lambda obj: slider_for_object(
+        obj, 'Alpha', 0, 1,
+        getter=lambda: 1 if obj.get_alpha() is None else obj.get_alpha()
+    ),
     'linewidth' : lambda obj: slider_for_object(obj, 'Line width', 0, 10),
-    'label': lambda obj: text_for_object(obj, 'Label')
+    'label': lambda obj: text_for_object(obj, 'Label'),
+    'linestyle': lambda obj: choice_combobox(obj, 'Line style', linestyle_dict),
+    'drawstyle': lambda obj: choice_combobox(obj, 'Draw style',
+                                             ['default', 'steps', 'steps-pre',
+                                              'steps-mid', 'steps-post'])
 }
+
 
 class Customizer(object):
     def __init__(self):
@@ -74,94 +109,129 @@ class Customizer(object):
         self.layout.addWidget(self.frame)
 
 
-def new_slider(name):
-    hbox_layout = QHBoxLayout()
-    label = QLabel(name)
-    label.setMinimumSize(QtCore.QSize(20, 0))
-    label.setAlignment(
-        QtCore.Qt.AlignRight |
-        QtCore.Qt.AlignTrailing |
-        QtCore.Qt.AlignVCenter)
 
-    slider = QSlider()
-    slider.setMouseTracking(False)
-    slider.setProperty("value", 0)
-    slider.setOrientation(QtCore.Qt.Horizontal)
-    slider.setInvertedAppearance(False)
-    slider.setInvertedControls(False)
-    slider.setTickPosition(QSlider.TicksAbove)
-    slider.setTickInterval(5)
+def test():
+    import pandas as pd
+    from datetime import datetime
+    series = pd.Series(
+        index=map(
+            datetime.utcfromtimestamp,
+            [ 0,  30,  31,  45,  46, 300]
+        ),
+        data=[0, 100,  10, 100,  10,  10]
+    ).resample('1s').mean().ffill()
+    ax = series.plot()
+    tree = Tree()
+    tree.new_tree({
+        'hej':ax.lines[0],
+        'toplevel_dt': datetime.utcnow(),
+    })
+    return tree, ax
 
-    value_label = QLabel('0')
-    value_label.setMinimumSize(QtCore.QSize(20, 0))
-    value_label.setAlignment(
-        QtCore.Qt.AlignRight |
-        QtCore.Qt.AlignTrailing |
-        QtCore.Qt.AlignVCenter)
+class Tree(QWidget):
+    def __init__(self):
+        super(Tree, self).__init__()
+        self.tw = QTreeWidget()
+        self.last_selected = None
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.tw)
+        self.show()
 
-    hbox_layout.addWidget(label)
-    hbox_layout.addWidget(slider)
-    hbox_layout.addWidget(value_label)
+    def new_tree(self, obj):
+        def add_top_level_item(tree_widget, item_obj, name):
+            top_item = QTreeWidgetItem()
+            top_item.target_obj = item_obj
+            tree_widget.addTopLevelItem(top_item)
+            logger.debug('Adding top level item: %s : %s', name, item_obj)
+            populate_tree_item(
+                top_item,
+                item_obj,
+                prefix='<b>{}</b> : '.format(name),
+                expand_objects=False
+            )
 
-    def update_text(val):
-        value_label.setText("%.2f" % (val / 100))
+        if isinstance(obj, dict):
+            for key, val in obj.iteritems():
+                add_top_level_item(self.tw, val, key)
+        else:
+            for attrname in dir(obj):
+                if attrname.startswith('_'):
+                    continue
+                else:
+                    add_top_level_item(tw, getattr(obj, attrname), attrname)
 
-    slider.valueChanged.connect(update_text)
+        self.tw.itemDoubleClicked.connect(expand_tree_item)
+        self.tw.itemSelectionChanged.connect(self.tree_item_selected)
 
-    return label, slider, value_label, hbox_layout
-
-
-def new_tree(obj):
-    tw = QTreeWidget()
-
-    def add_top_level_item(tree_widget, item_obj, name):
-        top_item = QTreeWidgetItem()
-        top_item.target_obj = item_obj
-        tree_widget.addTopLevelItem(top_item)
-        populate_tree_item(
-            top_item,
-            item_obj,
-            prefix='<b>{}</b> : '.format(name)
-        )
-
-    if isinstance(obj, dict):
-        for key, val in obj.iteritems():
-            add_top_level_item(tw, val, key)
-    else:
-        for attrname in dir(obj):
-            if attrname.startswith('_'):
-                continue
+    def tree_item_selected(self):
+        if hasattr(self.last_selected, 'prop_widget'):
+            self.last_selected.prop_widget.hide()
+        selected = self.tw.selectedItems()
+        if len(selected) > 1:
+            logger.debug('multiple items selected - no action')
+            return
+        else:
+            selected = selected[0]
+        if hasattr(selected, 'prop_widget'):
+            self.last_selected = selected
+            selected.prop_widget.show()
+        else:
+            maybe_widget = create_prop_widgets(selected.target_obj)
+            if maybe_widget is not None:
+                selected.prop_widget = maybe_widget
+                self.layout().addWidget(selected.prop_widget)
             else:
-                add_top_level_item(tw, getattr(obj, attrname), attrname)
-    tw.itemDoubleClicked.connect(expand_tree_item)
-    return tw
+                return
+
 
 
 def expand_tree_item(item, col):
-    obj = item.target_obj
-    if item.childCount() > 0:
-        print('Item is already expanded {}'.format(str(obj)[:100]))
+    if hasattr(item, 'target_obj'):
+        obj = item.target_obj
     else:
-        print(item, obj)
+        logging.debug('obj does not have target_obj attr: %s', obj)
+        return
+    if item.childCount() > 0:
+        logger.debug('Item is already expanded {}'.format(str(obj)[:100]))
+    else:
+        logger.debug('Expanding %s on tree item %s', obj, item)
         for attrname in dir(obj):
             if attrname.startswith('_'):
                 continue
             populate_tree_item(
                 item,
                 getattr(obj, attrname),
-                set_text=False,
+                text_already_set=True,
+                expand_objects=False,
             )
 
-def populate_tree_item(tree_item, obj, prefix='', set_text=True, recurse=True, curr_depth=0):
+
+def create_prop_widgets(obj):
+    # Add property-widget children if supported class
+    if type(obj) not in class_properties:
+        return None
+    container_widget = QFrame()
+    container_widget.setLayout( QVBoxLayout() )
+    props = class_properties.get(type(obj), [])
+    if props:
+        widget_factories = itemgetter(*props)(property_2_widget_factory)
+        for factory in widget_factories:
+            container_widget.layout().addWidget(factory(obj))
+    return container_widget
+
+
+def populate_tree_item(tree_item, obj, prefix='', text_already_set=False,
+                       expand_objects=True, curr_depth=0):
+    logger.debug('Populating from obj: %s', obj)
+    if not text_already_set:
+        text = format_obj(obj, prefix)
+        tree_item.treeWidget().setItemWidget(tree_item, 0, QLabel(text))
+        logger.debug('Setting text label: %s', text)
+
     if curr_depth > 3:
         print('hit depth limit')
         return
     elif isinstance(obj, (tuple, list, dict)):
-        text = '{pre}{typ} [{len}]'.format(
-                pre=prefix,
-                typ=type(obj),
-                len=len(obj),
-            )
         if isinstance(obj, dict):
             item_iter = sorted(obj.items())
         else:
@@ -175,8 +245,9 @@ def populate_tree_item(tree_item, obj, prefix='', set_text=True, recurse=True, c
                                item,
                                prefix='<b>{}</b> : '.format(key),
                                curr_depth=curr_depth + 1,
-                               recurse=False)
-    elif recurse:
+                               expand_objects=False)
+    elif expand_objects:
+        logger.debug('%s: recursing into %s', tree_item.target_obj, obj)
         for attrname in dir(obj):
             if attrname.startswith('_'):
                 continue
@@ -188,27 +259,14 @@ def populate_tree_item(tree_item, obj, prefix='', set_text=True, recurse=True, c
                 child_item,
                 item,
                 prefix='<b>{}</b> : '.format(attrname),
-                recurse=False,
+                expand_objects=False,
                 curr_depth=curr_depth + 1
-#                 set_text=False
+#                 text_already_set=False
             )
-
-    if set_text:
-        text = format_obj(obj, prefix)
-        container_widget = QFrame()
-        container_widget.setLayout(
-            QVBoxLayout()
-        )
-        container_widget.layout().addWidget(
-            QLabel(text)
-        )
-        print('setting text label: %s' % text)
-        props = class_properties.get(type(obj), [])
-        if props:
-            widget_factories = itemgetter(*props)(property_2_widget_factory)
-            for factory in widget_factories:
-                container_widget.layout().addWidget(factory(obj))
-        tree_item.treeWidget().setItemWidget(tree_item, 0, container_widget)
+    else:
+        child_item = QTreeWidgetItem()
+        child_item.target_obj = obj
+        tree_item.addChild(child_item)
 
 
 def deduce_getter_setter(obj, text, prop, getter, setter):
@@ -218,9 +276,11 @@ def deduce_getter_setter(obj, text, prop, getter, setter):
     setter = setter or getattr(obj, 'set_' + prop)
     return getter, setter
 
+
 def get_axes_from_object(obj):
     if hasattr(obj, 'axes'):
         return obj.axes
+
 
 def text_for_object(obj, text, prop=None, getter=None, setter=None):
     getter, setter = deduce_getter_setter(obj, text, prop, getter, setter)
@@ -239,15 +299,14 @@ def text_for_object(obj, text, prop=None, getter=None, setter=None):
     return obj_widget
 
 
-
 def slider_for_object(obj, text, min, max, prop=None, getter=None, setter=None):
     getter, setter = deduce_getter_setter(obj, text, prop, getter, setter)
-    label, slider, value_label, hbox_layout = new_slider(text)
+    label, slider, value_edit, hbox_layout = new_slider(text)
     obj_widget = QFrame()
     obj_widget.setLayout(hbox_layout)
 
-    def update(val):
-        setter(val / 100)
+    def update(value):
+        setter(value / 100)
         get_axes_from_object(obj).figure.canvas.draw_idle()
     slider.setValue(getter() * 100)
     slider.setMinimum(min * 100)
@@ -256,20 +315,87 @@ def slider_for_object(obj, text, min, max, prop=None, getter=None, setter=None):
 
     return obj_widget
 
-def test():
-    import pandas as pd
-    from datetime import datetime
-    series = pd.Series(
-        index=map(
-            datetime.utcfromtimestamp,
-            [ 0,  30,  31,  45,  46, 300]
-        ),
-        data=[0, 100,  10, 100,  10,  10]
-    ).resample('1s').mean().ffill()
-    ax = series.plot()
-    tw = new_tree({'hej':ax.lines})
-    tw.show()
-    return tw, ax
+
+def choice_combobox(obj, text, choices, prop=None, getter=None, setter=None):
+    getter, setter = deduce_getter_setter(obj, text, prop, getter, setter)
+    obj_widget = QFrame()
+    hbox = QHBoxLayout()
+    obj_widget.setLayout(hbox)
+    hbox.addWidget(QLabel(text))
+    combobox = QComboBox()
+    hbox.addWidget(combobox)
+    if not isinstance(choices, dict):
+        choices = OrderedDict(zip(choices, choices))
+    choice_list = choices.keys()
+    combobox.addItems(choice_list)
+    current_choice = getter()
+    combobox.setCurrentIndex(choices.values().index(current_choice))
+    def update(_placeholder):
+        new_choice = str(combobox.currentText())
+        if new_choice not in choices:
+            logging.error('Could not find among choices: %s (%s)',
+                          new_choice,
+                          choices)
+            return
+        setter(choices[new_choice])
+        get_axes_from_object(obj).figure.canvas.draw_idle()
+
+    combobox.currentIndexChanged.connect(update)
+    return obj_widget
+
+
+def new_slider(name):
+    hbox_layout = QHBoxLayout()
+    label = QLabel(name)
+    label.setMinimumSize(QtCore.QSize(20, 0))
+    label.setAlignment(
+        QtCore.Qt.AlignRight |
+        QtCore.Qt.AlignTrailing |
+        QtCore.Qt.AlignVCenter)
+
+    slider = QSlider()
+    slider.setMouseTracking(False)
+    slider.setProperty("value", 0)
+    slider.setOrientation(QtCore.Qt.Horizontal)
+    slider.setInvertedAppearance(False)
+    slider.setInvertedControls(False)
+    slider.setTickPosition(QSlider.TicksAbove)
+    slider.setTickInterval(5)
+
+    value_edit = QLineEdit('0')
+    value_edit.setMinimumSize(QtCore.QSize(20, 0))
+    value_edit.setAlignment(
+        QtCore.Qt.AlignRight |
+        QtCore.Qt.AlignTrailing |
+        QtCore.Qt.AlignVCenter)
+
+    hbox_layout.addWidget(label)
+    hbox_layout.addWidget(slider)
+    hbox_layout.addWidget(value_edit)
+
+    def on_slider_changed(val):
+        text = "%.2f" % (val / 100)
+        if str(value_edit.text()) != text:
+            logger.debug('not equal text %s %s', value_edit.text(), text)
+            value_edit.setText(text)
+
+    def on_box_changed():
+        text = value_edit.text()
+        try:
+            value = float(text) * 100
+        except ValueError as ve:
+            logger.error('Could not convert argument to flaot %s', text)
+            return
+        logger.debug('updating slider %s', text)
+        if value > slider.maximum():
+            slider.setMaximum(value)
+        slider.setValue(value)
+
+    slider.valueChanged.connect(on_slider_changed)
+    value_edit.editingFinished.connect(on_box_changed)
+
+    return label, slider, value_edit, hbox_layout
+
 
 
 def format_obj(obj, prefix):
